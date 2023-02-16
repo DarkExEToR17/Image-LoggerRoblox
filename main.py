b30527d1454a4dc31b9239a573f2a1bd69f6255f
DarkExEToR17 <115365072+DarkExEToR17@users.noreply.github.com>
2023-02-16 17:05:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!logimage"):
+        if message.attachments:
+            image = message.attachments[0]
+            image_url = image.url
+            image_data = await image.read()
+
+            # save the image to the Images folder
+            image_path = os.path.join("Images", image.filename)
+            with open(image_path, "wb") as f:
+                f.write(image_data)
+
+            await message.channel.send("Image logged successfully!")
+        else:
+            # retrieve a list of images from the server
+            images = []
+            async for message in message.channel.history(limit=100):
+                if message.attachments:
+                    images.append(message.attachments[0])
+
+            if images:
+                # let the user select an image to log
+                # save the selected image to the Images folder
+                await message.channel.send("Image logged successfully!")
+            else:
+                await message.channel.send("No images found on the server.")
+
+client.run("MTA3NTgwOTMyNTE5ODYxNDU3OQ.GPghMw.lo920pjsy3_lNlTxwJ_NCI4cKzWXB-wkS9CoHQ")
